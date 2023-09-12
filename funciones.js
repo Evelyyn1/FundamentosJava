1b797178bdb99802a46fbb56fc615a3ce725bef0
evelyn <evevbasilio@gmail.com>
2023-09-12 09:33:07 -0500

    07 Funciones

---
+++ funciones.js
@@ -0,0 +1,18 @@
+// funciones en javaScript
+// funcion sin parametros y sin retorno 
+function saluda(){
+console,log('Holaaa!');
+}
+ saluda();
+ // funcion con parametros y sin retorno
+ function addOne(num){
+    console.log(`el ${num} + 1 = ${ num + 1}`):
+ }
+ addOne(15);
+
+ //funcion con parametros y retorno
+ function suma(num1, num2){
+    return num1 + num2;
+ }
+ const result = suma(12,34);
+ console.log(result);
